1c7bcf9ed3f0cee890a9ae916acfa8737376d7bd
Mauricio Herrera <mauherrerag@email.arizona.edu>
2020-12-08 16:57:30 -0700

    Added toString method

--- src/main/java/edu/arizona/cs/ResultClass.java
+++ src/main/java/edu/arizona/cs/ResultClass.java
@@ -4,4 +4,8 @@ import org.apache.lucene.document.Document;
 public class ResultClass {
     Document docName;
     double docScore = 0;
+
+    public String toString() {
+        return docName.toString() + " score: " + docScore;
+    }
 }
